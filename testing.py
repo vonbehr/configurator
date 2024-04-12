efb01d727e6a243b23eb92a54656d074d3624846
Florian von Behr <florian@florianvonbehr.de>
2024-04-12 14:56:28 +0200

    Update testing.py
    tada

--- testing.py
+++ testing.py
@@ -1,11 +1,15 @@
 from colormath.color_objects import sRGBColor
 import colormath
 
-hex = "#cb563b"
+# hex_list = ["#cb563b"]
 
+# hex = hex_list[0][1:]
+# print(hex)
+
+hex = "cb563b"
 rgb = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
 print(rgb)
 
 srgb = colormath.color_objects.sRGBColor.new_from_rgb_hex(hex)
 print(srgb)
-print(type(colormath.color_objects.sRGBColor.get_value_tuple(srgb)))
+print(colormath.color_objects.sRGBColor.get_value_tuple(srgb))
