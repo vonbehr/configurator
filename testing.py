470d57e5d212b336d0114c1055dead2e12c68657
Florian von Behr <florian@florianvonbehr.de>
2024-05-31 11:05:25 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,11 +1,37 @@
-import os
+import psapi
+import numpy as np
+import cv2
 
-input_dir = "//thescopefiles/projects/24028_BMW/shots/BMWi7Config01/publish/render"
-exr_files = []
+document_color_mode = psapi.enum.ColorMode.rgb
+width = 4000
+height = 2250
+file = psapi.LayeredFile_8bit(document_color_mode, width, height)
 
-for dirpath, subdirs, files in os.walk(input_dir):
-    for i in files:
-        if i.endswith(".exr"):
-            exr_files.append(os.path.join(dirpath, i))
+# CV2 reads images in packed BGR order by default, we now need to go from packed to planar
+# e.g. from BGR BGR BGR BGR -> RRRR BBBB GGGG
+image = cv2.imread("C:/Users/florianbehr/Desktop/configurator/The_Scope_Polaroid_Team_Florian.jpg")
 
-print(exr_files)
+transformed_image = np.zeros((image.shape[2], image.shape[0], image.shape[1]), np.uint8)
+transformed_image[0] = image[:, :, 2]   # Use numpy slicing to get the third channel of bgr
+transformed_image[1] = image[:, :, 1]
+transformed_image[2] = image[:, :, 0]
+
+img_lr = psapi.ImageLayer_8bit(
+        transformed_image,
+        layer_name="layer 001",
+        width=width,
+        height=height,
+        color_mode=document_color_mode)
+
+group_layer = psapi.GroupLayer_8bit(
+        layer_name="Groupie",
+        width=width,
+        height=height,
+        color_mode=document_color_mode)
+
+# add layer to group
+group_layer.add_layer(file, img_lr)
+
+# Add to the file and write out
+file.add_layer(group_layer)
+file.write("C:/Users/florianbehr/Desktop/configurator/Out_grp.psd")
