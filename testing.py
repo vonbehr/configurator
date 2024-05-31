3a46483c8535fc9b0ca5e8161f04f188134bf2e9
Florian von Behr <florian@florianvonbehr.de>
2024-05-31 17:51:53 +0200

    Update testing.py
    Save PS layer as image file to disc

--- testing.py
+++ testing.py
@@ -1,37 +1,51 @@
 import psapi
 import numpy as np
 import cv2
+import numpy as np
 
-document_color_mode = psapi.enum.ColorMode.rgb
-width = 4000
-height = 2250
-file = psapi.LayeredFile_8bit(document_color_mode, width, height)
+filepath = "C:/Users/florianbehr/Desktop/configurator/Colorchart.psd"
 
-# CV2 reads images in packed BGR order by default, we now need to go from packed to planar
-# e.g. from BGR BGR BGR BGR -> RRRR BBBB GGGG
-image = cv2.imread("C:/Users/florianbehr/Desktop/configurator/The_Scope_Polaroid_Team_Florian.jpg")
+ps_document = psapi.LayeredFile.read(filepath)
 
-transformed_image = np.zeros((image.shape[2], image.shape[0], image.shape[1]), np.uint8)
-transformed_image[0] = image[:, :, 2]   # Use numpy slicing to get the third channel of bgr
-transformed_image[1] = image[:, :, 1]
-transformed_image[2] = image[:, :, 0]
+# image = cv2.imread("C:/Users/florianbehr/Desktop/configurator/r.jpg", cv2.IMREAD_UNCHANGED)
+# print(type(image))
 
-img_lr = psapi.ImageLayer_8bit(
-        transformed_image,
-        layer_name="layer 001",
-        width=width,
-        height=height,
-        color_mode=document_color_mode)
+# image_array = dict(enumerate(image.flatten(), 1))
+# print(image_array)
 
-group_layer = psapi.GroupLayer_8bit(
-        layer_name="Groupie",
-        width=width,
-        height=height,
-        color_mode=document_color_mode)
+# print(ps_document.width)
+# print(ps_document.height)
 
-# add layer to group
-group_layer.add_layer(file, img_lr)
+for layer in ps_document.layers:
+    # print(type(layer))
 
-# Add to the file and write out
-file.add_layer(group_layer)
-file.write("C:/Users/florianbehr/Desktop/configurator/Out_grp.psd")
+    if isinstance(layer, psapi.GroupLayer_8bit):
+        print("Group Layer")
+        # for sub_layer in layer.layers:
+        #     name = sub_layer.name
+
+        #     transformed_image = np.zeros((sub_layer.shape[0], sub_layer.shape[1], sub_layer.shape[2]), np.uint8)
+        #     transformed_image[2] = image[:, :, 0]   # Use numpy slicing to get the third channel of bgr
+        #     transformed_image[1] = image[:, :, 1]
+        #     transformed_image[0] = image[:, :, 2]
+        #     # transformed_image[3] = image[:, :, 3]
+
+
+        #     cv2.imwrite("C:/Users/florianbehr/Desktop/configurator/" + name + ".jpg", transformed_image)
+
+    elif isinstance(layer, psapi.ImageLayer_8bit):
+        name = layer.name
+        # print(layer.image_data)
+        # for key, value in layer.image_data.items():
+        #     print(key)
+        #     print(value)
+
+
+        R = layer.image_data[0]
+        G = layer.image_data[1]
+        B = layer.image_data[2]
+        A = layer.image_data[-1]
+
+        packed_array = np.dstack((B, G, R, A))
+
+        cv2.imwrite("C:/Users/florianbehr/Desktop/configurator/" + name + ".png", packed_array)
